8317fe0837169a131ba457240eca92bb251a0502
Matthieu Moy <Matthieu.Moy@imag.fr>
2010-11-16 16:00:51 +0100

    Makefile to call the scripts correctly and generate a tarball

--- exam-expl/exam-test.sh
+++ exam-expl/exam-test.sh
@@ -16,6 +16,9 @@ exam_dbuser=moy
 # Host where the database is located
 exam_dbhost=arpont.imag.fr
 
+# Database password (will be prompted interactively if not specified)
+exam_dbpass=insertdbpasswordhere
+
 # Database to use (mysql/postgresql)
 exam_dbtype=mysql
 

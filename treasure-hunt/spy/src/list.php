54389de906e619b02ebe267da0c1eeb85aa4c5a6
Matthieu Moy <Matthieu.Moy@imag.fr>
2011-07-20 10:56:31 +0200

    Show number of accesses for each step

--- treasure-hunt/spy/src/list.php
+++ treasure-hunt/spy/src/list.php
@@ -8,10 +8,14 @@ exam_header('Treasure Hunt monitoring');
 
 echo '<script src="./sorttable.js"></script>';
 
-echo '<p>Click on table headers to sort</p>';
+echo '<p>Click on table headers to sort.</p>';
 
 // http://www.php.net/manual/en/function.mysql-query.php#81348
-function echo_result($result) {
+// $format = tableau pour formatter l'affichage.
+//    $format[$no_colonne] est une chaine de formattage :
+//    - %s est remplacé par le contenu de la case.
+//    - %b est remplacé par le contenu en remplacant les " " par &nbsp;
+function echo_result($result, $format=null) {
 	?><table class="sortable"><tr><?
 	if(! $result) { ?><th>result not valid</th><? }
 	else {
@@ -30,7 +34,16 @@ function echo_result($result) {
 		} else
 			while($row=mysql_fetch_assoc($result)) {
 				?><tr style="white-space:nowrap"><?
-				foreach($row as $key=>$value) { ?><td><?php echo htmlspecialchars($value); ?></td><? }
+				foreach($row as $key=>$value) { 
+					$value = htmlspecialchars($value);
+					if ($format[$key]) {
+						$snbsp = str_replace(" ", "&nbsp;", $value);
+						$value = str_replace(array("%b", "%s"),
+								     array($snbsp, $value),
+								     $format[$key]);
+					}
+					echo '<td>'. $value .'</td>';
+				}
 				?></tr><?
 			}
 	}
@@ -45,32 +58,33 @@ while($row=mysql_fetch_assoc($result)) {
 	$step = $row['step'];
 	if (preg_match('/^[a-zA-Z][a-zA-Z0-9]*$/', $step)) {
 		// ignore potentially harmfull step names for security reason
-		$step_fields .= ', MIN('. $step .'.date) as '. $step .'_date';
-		$step_join .= 'LEFT JOIN hunt_access as '. $step .'
- ON (reference.login = '. $step .'.login
- AND '. $step .".step = '". $step ."')";
+		$step_fields .= ',
+  MIN('. $step .'.date) as '. $step .'_date, COUNT(DISTINCT '. $step .'.date) as '. $step .'_nb';
+		$step_join .= '
+LEFT JOIN hunt_access as '. $step .'
+  ON (reference.login = '. $step .'.login AND '. $step .".step = '". $step ."')";
 	}
 }
 
 echo '<h2>Registered students accesses</h2>';
 
 $query = 'SELECT reference.*'. $step_fields .'
-FROM hunt_student AS reference
-'. $step_join .'
+FROM hunt_student AS reference'. $step_join .'
 GROUP BY reference.login;';
+// echo '<pre>'. $query .'</pre>';
 $result = exam_query($query);
-echo_result($result);
+echo_result($result, array('login' => '<a href="http://intranet.ensimag.fr/ZENITH/affiche-etu.php?ETU=%s">%s</a>'));
 
 echo '<h2>Unregistered students accesses</h2>';
 
 $query = 'SELECT reference.login'. $step_fields .'
-FROM hunt_access AS reference
-'. $step_join .'
-WHERE reference.login NOT IN (SELECT login FROM hunt_student)
+FROM (SELECT DISTINCT login
+      FROM hunt_access
+      WHERE login NOT IN (SELECT login FROM hunt_student)) AS reference'. $step_join .'
 GROUP BY reference.login;';
-//echo $query;
+// echo '<pre>'. $query .'</pre>';
 $result = exam_query($query);
-echo_result($result);
+echo_result($result, array('login' => '<a href="http://intranet.ensimag.fr/ZENITH/affiche-etu.php?ETU=%s">%s</a>'));
 
 exam_footer();
 ?>

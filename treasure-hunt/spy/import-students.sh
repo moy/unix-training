f934bb68226d9f35d8c887ddbd2b6768cd28194a
Matthieu Moy <Matthieu.Moy@imag.fr>
2011-11-15 14:53:18 +0100

    spy/import-students: either insert or update (override) existing.

--- treasure-hunt/spy/import-students.sh
+++ treasure-hunt/spy/import-students.sh
@@ -7,11 +7,13 @@ Options:
 	--help		This help message.
 	--apply		Apply SQL directly to the database
 	--postgresql	Use PostgreSQL instead of MySQL
+	--group	GROUP	Group to use (default=1A)
 EOF
 }
 
 apply=no
 group=1A
+mode=insert
 
 while test $# -ne 0; do
     case "$1" in
@@ -29,7 +31,10 @@ while test $# -ne 0; do
 	    shift
 	    group="$1"
 	    ;;
-       *)
+	"--update")
+	    mode=update
+	    ;;
+	*)
             echo "unrecognized option $1"
             usage
             exit 1
@@ -44,6 +49,18 @@ grep '[0-9].*;.*;.*;.*;.*;' | while read line; do
     login=$(echo "$line" | cut -d \; -f 2 | sed "s/'/\\\\'/g")
     first=$(echo "$line" | cut -d \; -f 4 | sed "s/'/\\\\'/g")
     last=$( echo "$line" | cut -d \; -f 5 | sed "s/'/\\\\'/g")
-    echo 'INSERT INTO `hunt_student` (`login` , `group`, `first_name` , `familly_name`)'
-    echo "                    VALUES ('$login', '$group', '$first',      '$last');"
+    case $mode in
+	"insert")
+	    echo 'INSERT INTO `hunt_student` (`login` , `group`, `first_name` , `familly_name`)'
+	    echo "                    VALUES ('$login', '$group', '$first',      '$last');"
+	    ;;
+	"update")
+	    echo "UPDATE \`hunt_student\` SET \`group\`='$group', \`first_name\`='$first' , \`familly_name\`='$last'"
+	    echo "WHERE \`login\`='$login';"
+	    ;;
+	*)
+	    echo "No such mode $mode." 1>&2
+	    exit 1
+	    ;;
+    esac
 done | $db_cmd

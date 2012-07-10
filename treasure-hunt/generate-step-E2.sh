07e8be89f7a4a7d69911197d49fa72b11055a658
Matthieu Moy <Matthieu.Moy@imag.fr>
2012-07-10 22:00:47 +0200

    Remove hardcoded URL from step E2

--- treasure-hunt/generate-step-E2.sh
+++ treasure-hunt/generate-step-E2.sh
@@ -1,4 +1,10 @@
-Bien joué !
+#! /bin/sh
+
+. ./treasure-setup.sh
+. ./spy-lib.sh
+. ./i18n-lib.sh
+
+gettext 'Bien joué !
 
 Les fichiers cachés sont simplement ceux dont le nom commence par un
 point, on peut les manipuler comme les autres, mais "ls" ne les
@@ -7,4 +13,5 @@ affiche pas. Par contre, "ls -a" les affiche.
 Les étapes suivantes se trouvent dans une archive TAR compressée,
 disponible ici :
 
-http://www-verimag.imag.fr/~moy/jeu-de-piste/etape-E3.tar.gz
+$web_url/etape-E3.tar.gz
+' | envsubst > dot-$(gettext etape)-E2.txt

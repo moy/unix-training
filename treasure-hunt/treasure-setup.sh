0407b643cefd2c033f5006c2fd5d6ef3217fc045
Matthieu Moy <Matthieu.Moy@imag.fr>
2012-07-10 16:24:28 +0200

    some documentation in treasure-setup.sh

--- treasure-hunt/treasure-setup.sh
+++ treasure-hunt/treasure-setup.sh
@@ -1,4 +1,18 @@
+# The main machine is the one on which students usually work.
+# Students must have access to a shared directory, to which
+# we'll upload files corresponding to the steps of the hunt.
 mainmachine=telesun.imag.fr
+
+# Directory containing many steps of the hunt. You must have write
+# access to it, and students must have read access (but won't be able
+# to list the content of the directory).
+# Warning: the path is hardcoded in many steps.
 maindir=/home/perms/moy/jeu-de-piste/
+
+# Other machine on which students have access. It will be used to
+# practice various basic networking tools (SSH, sftp, ...)
 auxiliarymachine=ensilinux.imag.fr
+
+# The content of this directory should be uploaded to a website (whose
+# URL is currently hardcoded in the steps).
 web=$HOME/WWW/jeu-de-piste

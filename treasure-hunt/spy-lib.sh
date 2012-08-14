c99e4ff35a35ccf17b7a05e5bf8804d5a87ae389
Matthieu Moy <Matthieu.Moy@imag.fr>
2012-08-14 16:43:10 +0200

    spy: don't escape $ in $LOGNAME
    otherwise, the monitoring system actually sees $LOGNAME instead of the
    student's login.

--- treasure-hunt/spy-lib.sh
+++ treasure-hunt/spy-lib.sh
@@ -3,6 +3,6 @@
 . ./treasure-setup.sh
 
 monitor_step_cmd () {
-    printf 'wget "%s/record.php?login=\$LOGNAME&step=%s" -O /dev/null 2>/dev/null &\n' \
+    printf 'wget "%s/record.php?login=$LOGNAME&step=%s" -O /dev/null 2>/dev/null &\n' \
 	"$spy_url" "$1"
 }

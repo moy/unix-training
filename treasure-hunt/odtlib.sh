8fe279022f679366f935acb8fa807d0bb1c0a12c
Matthieu Moy <Matthieu.Moy@imag.fr>
2012-10-16 21:53:03 +0200

    txt2odt: generate files with correct mimetype (so that 'file' can tell it)

--- treasure-hunt/odtlib.sh
+++ treasure-hunt/odtlib.sh
@@ -21,6 +21,11 @@ txt2odt () {
     unzip -q "$HUNT_DIR"/template.odt
     perl -pi -e "s/TEXT_WILL_BE_INSERTED_HERE/$text/" content.xml
     cd ..
-    (cd template.$$ && zip -q -r - * ) > "$1"
+    # Put mimetype first and uncompressed so that "file foo.odt" says
+    # it's an OpenDocument text:
+    # http://www.jejik.com/articles/2010/03/how_to_correctly_create_odf_documents_using_zip/
+    (cd template.$$ && 
+	zip -q -0 -X tmp.odt mimetype && zip -q -r tmp.odt * -x mimetype )
+    mv -f template.$$/tmp.odt "$1"
     rm -fr template.$$
 }

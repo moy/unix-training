dd859a8b9e575dc6cb538066b40c7ab9f57a1f42
Matthieu Moy <Matthieu.Moy@imag.fr>
2013-11-11 15:54:26 +0100

    reindent decoder*.c

--- treasure-hunt/decoder_bis.c
+++ treasure-hunt/decoder_bis.c
@@ -4,53 +4,65 @@
 #include <stdio.h>
 #include <string.h>
 
-char tchar(char one){
-   if ('1' <= one && one <= '6')
-    return (one + 48);
-   if ('7' <= one && one <= '9')
-    return (one - 6);
-   if (one == '0') return ('4');
-   if ('x' <= one && one <= 'z')
-     return (one - 67);
-   if (one == 't') return ('8');
-   if (one == 'a') return ('9');
-   if (one == 'b') return ('0');
-   if (one == 'c') return ('x');
-   if (one == 'd') return ('y');
-   if (one == 'e') return ('z');
-   if (one == 'f') return ('t');
-  
-  return one;
+char tchar(char one)
+{
+	if ('1' <= one && one <= '6')
+		return (one + 48);
+	if ('7' <= one && one <= '9')
+		return (one - 6);
+	if (one == '0')
+		return ('4');
+	if ('x' <= one && one <= 'z')
+		return (one - 67);
+	if (one == 't')
+		return ('8');
+	if (one == 'a')
+		return ('9');
+	if (one == 'b')
+		return ('0');
+	if (one == 'c')
+		return ('x');
+	if (one == 'd')
+		return ('y');
+	if (one == 'e')
+		return ('z');
+	if (one == 'f')
+		return ('t');
+
+	return one;
 }
 
-void translate (char input[512], char output[512] ){
-  int i = 0;
-  while (i<512 && input[i] != '\0') {
-    output[i] = tchar(input[i]);
-    i++;
-  }
-  output[i] = '\0';
+void translate(char input[512], char output[512])
+{
+	int i = 0;
+	while (i < 512 && input[i] != '\0') {
+		output[i] = tchar(input[i]);
+		i++;
+	}
+	output[i] = '\0';
 }
 
 
-void dprint(char *str) {
-  char *w;
-  char output[512];
-  w = strtok(str," \n");
-  while (w!=NULL){
-    translate(w,output);
-    printf("%s ",output);
-    w=strtok(NULL," \n");
-  }
-  printf("\n");
+void dprint(char *str)
+{
+	char *w;
+	char output[512];
+	w = strtok(str, " \n");
+	while (w != NULL) {
+		translate(w, output);
+		printf("%s ", output);
+		w = strtok(NULL, " \n");
+	}
+	printf("\n");
 }
 
-int main(void) {
-  char line[512];
-  gets(line);
-  while ( !feof(stdin) ) {
-    dprint(line);
-    gets (line);
-  }
-  return 0;
+int main(void)
+{
+	char line[512];
+	gets(line);
+	while (!feof(stdin)) {
+		dprint(line);
+		gets(line);
+	}
+	return 0;
 }

957d8a4c09902d6d8da2f7db8f76e8f726cb57e9
Matthieu Moy <Matthieu.Moy@imag.fr>
2013-11-15 18:50:44 +0100

    Fix incompatibility with recent PHP versions
    My use of <?php ?> in the middle of a function seems to confuse PHP 5.3,
    which errors out (unexpected end of file at end of common.php).

--- gen-exam/php/inc/common.php
+++ gen-exam/php/inc/common.php
@@ -2,21 +2,20 @@
 defined('_VALID_INCLUDE') or die('Direct access not allowed.');
 
 function exam_header($title = '', $basedir = '.') {
-	echo '<'.'?xml version="1.0" encoding="UTF-8" ?'.'>';
- 	?>
+	echo '<'.'?xml version="1.0" encoding="UTF-8" ?'.'>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 	<meta name="author" content="Matthieu Moy" />
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $title ?></title>
+	<title>'. $title .'</title>
 	<style type="text/css">
-	@import "<?php echo $basedir ?>/style.css";
+	@import "'. htmlspecialchars($basedir) .'/style.css";
 	</style>
 </head>
 <body>
-	<?
+';
 	  if ($title != '') {
 		  echo "<h1>$title</h1>";
 	  }

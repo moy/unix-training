c7e736de7117026a9b9801175056217f6baa52f1
Matthieu Moy <Matthieu.Moy@imag.fr>
2015-03-20 20:56:01 +0100

    python_big_string, improve python_big_array

--- gen-exam/python-lib.sh
+++ gen-exam/python-lib.sh
@@ -13,17 +13,37 @@ $1"
 # Print a big array literal
 python_big_array() {
     printf '['
-    for i in $(seq 9); do
+    printf '\n    1,2,     4,   -23,    5,      5432,                       666,   108,'
+    for i in $(seq 9) $(seq 9); do # We can't go above 9 to allow dechashes[$i$j].
 	printf '\n    '
 	for j in $(seq 0 $(dechash_bound "$1$i" 40)); do
 	    printf '%s, ' "${dechashes[$i$j]}"
 	done
-	if [ $i = 6 ]
+	if [ $i = 8 ]
 	then
-	    printf '\n    7175, 5648, 2345, 42, 105, 8447, 2562, 9944, 1677, 6995,'
+	    printf '\n    7175, 5648, 2345, 42, 105, -8447, 2562, 9944, 1677, 6995,'
 	fi
     done
+    printf '\n    1,2,\n    4,\n    23,    -555,      5432,                       -666,   114,'
     echo "42]" 
+}
+
+python_big_string() {
+    printf '("blablabla"'
+    for i in $(seq 9) $(seq 9); do
+	for j in $(seq 9); do
+	    printf '+ "%s"' "${hashes[$i]}${dechashes[$i$j]}ii"
+	done
+	if [ $i = 7 ]
+	then
+	    printf '+ "%s" + "%s"' "${hashes[$i]}ic" "i${dechashes[$i$j]}"
+	fi
+	for j in $(seq 9); do
+	    printf '+ "%s"' "${hashes[$j]}i${dechashes[$i$j]}"
+	done
+	printf '\n'
+    done
+    printf ")"
 }
 
 python_numpy_array() {

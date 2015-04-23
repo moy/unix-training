819af99a2b55b58dc10e71fa90057adb04dbbe20
Matthieu Moy <Matthieu.Moy@imag.fr>
2015-04-23 20:48:36 +0200

    HTML5 content-type

--- gen-exam/php/inc/common.php
+++ gen-exam/php/inc/common.php
@@ -14,13 +14,12 @@ if (get_magic_quotes_gpc()) {
 }
 
 function exam_header($title = '', $basedir = '.') {
-	echo '<'.'?xml version="1.0" encoding="UTF-8" ?'.'>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	echo '<!DOCTYPE html>
+<html>
 <head>
-	<meta name="author" content="Matthieu Moy" />
+	<meta charset="UTF-8">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="author" content="Matthieu Moy" />
 	<title>'. $title .'</title>
 	<style type="text/css">
 	@import "'. htmlspecialchars($basedir) .'/style.css";
